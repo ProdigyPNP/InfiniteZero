3c630bfa051058296599ca9fa7e7384b24264d0d
gemsvido <gemsvido@gmail.com>
2022-07-22 23:51:10 -0700

    Unique IP tracking cuz I'm cool like that

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,12 +1,38 @@
+import * as fs from "fs";
+
 export function Analytics (req : any) {
 
+    AddUniqueIP(req);
+    console.log(request(req));
+
+}
+
+function request (req : any) : string {
 
     const IP : string = req.ip.toString();
     const UserAgent : string = new String(req.headers["user-agent"]).valueOf();
     const DateTime : string = new Date(Date.now()).toString();
     const Path : string = req.path.toString();
 
+    const out : string = JSON.stringify({IP, UserAgent, DateTime, Path});
 
-    console.log(JSON.stringify({IP, UserAgent, DateTime, Path}));
+    
+
+    return out;
+}
+
+
+
+function AddUniqueIP (req : any) : boolean {
+
+    const IP : string = req.ip.toString();
+    const AllIPs : string[] = fs.readFileSync("./analytics/UniqueIPs.txt", "utf8").split("\n");
+
+    if (AllIPs.includes(IP)) {
+        return false;
+    } else {
+        fs.appendFileSync("./analytics/UniqueIPs.txt", IP + "\n");
+        return true;
+    }
 
 }
